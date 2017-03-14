76d57548f785407213d6a3b095067c24ba5f8011
Richard Dotchin <richard.dotchin@gmail.com>
2017-03-14 02:13:13 -0400

    parsing entire directory and checking if files are js

--- test/nodeGitTest.js
+++ test/nodeGitTest.js
@@ -1,7 +1,26 @@
-var Git = require("nodegit");
-
+const Git = require("nodegit");
+const execFile = require('child_process').execFile;
+const fs = require('fs');
 /* Test nodegit by cloning a GitHub repo*/
-Git.Clone("https://github.com/rdotchin/angular-to-do", "angtodo").then(function(repository) {
+/*Git.Clone("https://github.com/rdotchin/angular-to-do", "angtodo").then(function(repository) {
   // Work with the repository object here.
   console.log("success");
+});*/
+
+//find all files in angtodo 
+execFile('find', [ 'angtodo' ], function(err, stdout, stderr) {
+  //split the results with a space
+  var file_list = stdout.split('\n');
+  var arr = [];
+  arr.push(file_list);
+
+  //for each file search if it includes .js but doesn't include bower(to reduce results)
+  file_list.forEach(function(element){
+  	if (element.includes(".js") && !element.includes("bower")) {
+  		console.log(element);
+  	}
+  });
+
+
 });
+
