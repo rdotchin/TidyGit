181c88f50f2926e02756a954429a4a814dbe8063
Richard Dotchin <richard.dotchin@gmail.com>
2017-03-24 23:17:17 -0400

    cleaned up server.js

--- server.js
+++ server.js
@@ -10,13 +10,7 @@ const PORT = process.env.PORT || 8080;
 
 
 /*================================PASSPORT GITHUB=================================================*/
-// Passport session setup.
-//   To support persistent login sessions, Passport needs to be able to
-//   serialize users into and deserialize users out of the session.  Typically,
-//   this will be as simple as storing the user ID when serializing, and finding
-//   the user by ID when deserializing.  However, since this example does not
-//   have a database of user records, the complete GitHub profile is serialized
-//   and deserialized.
+
 passport.serializeUser(function(user, done) {
     /*  console.log(user);*/
     console.log('serialize', user);
